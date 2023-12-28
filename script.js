8541c5d69be227ce620e4f97f933db44d37596f9
Yihun Alemayehu <yankure01@gmail.com>
2023-12-28 10:37:48 +0300

    small update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Engineer","Web-developer", "Mobile app Developer", "Graphics Designer", "Freelancer"],
+        strings: ["Flutter developer","Software Engineer", "Video Editor", "Graphics Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Engineer","Web-developer", "Mobile app Developer", "Graphics Designer", "Freelancer"],
+        strings:  ["Flutter developer","Software Engineer", "Video Editor", "Graphics Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
